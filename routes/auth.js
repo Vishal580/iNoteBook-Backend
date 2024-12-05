4f3c1847216339f95411629423d7284f3042f259
Vishal580 <R20200530.VISHAL@cse.reva.edu.in>
2024-12-05 17:07:45 +0530

    Storing Data into the Database Using Mongoose Model

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const User = require('../models/User')
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-    res.json([])
+// creating a user using: POST "/api/auth/". It Doesn't require auth 
+router.post('/', (req, res)=>{
+    console.log(req.body);
+    const user = User(req.body);
+    user.save()
+    res.send(req.body);
 })
 
 module.exports = router
