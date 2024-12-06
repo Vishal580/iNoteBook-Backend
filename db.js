758d598df21f8b50de334174e30c232214734a7c
Vishal580 <R20200530.VISHAL@cse.reva.edu.in>
2024-12-06 18:05:53 +0530

    Adding Data Validation Using express-validator

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const mongooseURI = "mongodb://localhost:27017/"
+const mongooseURI = "mongodb://localhost:27017/inotebook"
 
 // const connectToMongo = () => {
 //     mongoose.connect(mongooseURI, () =>{
